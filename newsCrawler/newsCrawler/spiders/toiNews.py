2b411980e3f4993f4fe8352edfed1136811c2e5a
Aayush <patni.aayush1998@gmail.com>
2020-04-25 17:49:19 +0530

    Initial Commit

---
+++ newsCrawler/newsCrawler/spiders/toiNews.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from newspaper import Article
+
+class ToinewsSpider(scrapy.Spider):
+    name = 'toiNews'
+    allowed_domains = ['https://timesofindia.indiatimes.com/india']
+    start_urls = ['https://timesofindia.indiatimes.com/india/']
+
+    def parse(self, response):
+        news_url = response.css("span.w_tle > a::attr(href)").extract()
+        news_title = response.css("span.w_tle > a::attr(title)").extract()
+        for url, title in zip(news_url, news_title):
+            if(str(url).startswith('http')):
+                news = Article(url)
+                try:
+                    news.download()
+                except:
+                    print("Failed to download")
+                    continue
+                news.parse()
+                news.nlp()
+                scraped_info = {
+                    'url' : url,
+                    'title' : title,
+                    #'text' : news.text,
+                    'publish_date' : news.publish_date,
+                    'keywords' : news.keywords
+                }
+                yield scraped_info
